1326a61c85ba1db95e98ee1df5b8fba7fabb3c8d
marszabo <96.mark.szabo@gmail.com>
2021-07-28 19:28:46 +0200

    Added the icons of the toolbar.

--- gitgud/ui.py
+++ gitgud/ui.py
@@ -1,5 +1,7 @@
+from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QGridLayout, QLabel, QMenuBar, QPushButton, QStatusBar, QToolBar, QToolButton, QWidget, QMainWindow
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QGridLayout, QLabel, QMenu, QMenuBar, QPushButton, QStatusBar, QToolBar, QToolButton, QWidget, QMainWindow
 
 
 class GitGudUi(QMainWindow):
@@ -20,4 +22,30 @@ class GitGudUi(QMainWindow):
         openAction = fileMenu.addAction("Open")
 
     def _addToolBar(self):
-        self.toolBar = self.addToolBar("Tool bar")
+        self.toolBar = self.addToolBar("toolBar")
+        self.toolBar.setStyleSheet(
+            "background-color: #052f4d;"+
+            "color: white;"
+            )
+        refreshButton = self._createToolButton(self.toolBar,"gitgud/assets/refresh.png","Refresh")
+        pushButton = self._createToolButton(self.toolBar,"gitgud/assets/push.png","Push")
+        pullButton = self._createToolButton(self.toolBar,"gitgud/assets/pull.png","Pull")
+        stashButton = self._createToolButton(self.toolBar,"gitgud/assets/stash.png","Stash")
+        popButton = self._createToolButton(self.toolBar,"gitgud/assets/pop.png","Pop")
+        """        
+        pullMenu = QMenu("Kappa")
+        pullMenu.addAction("Fetch")
+        pullButton.setMenu(pullMenu)
+        pullMenu
+        pullButton.setPopupMode(QToolButton.InstantPopup)
+        """
+
+
+
+    def _createToolButton(self,toolBar,icon,buttonText):
+        button = QToolButton(self)
+        button.setIcon(QIcon(icon))
+        button.setText(buttonText)
+        button.setToolButtonStyle(QtCore.Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
+        toolBar.addWidget(button)
+        return button
